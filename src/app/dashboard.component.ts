b85fd8e24fd94f1242c19d90cce303634908271d
Ernie <ernest.homsy@parsons.com>
2016-11-29 14:48:51 -0500

    add un-styled heroes dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroService} from "./hero.service";
+import {Hero} from "./hero";
 
 @Component({
   selector: 'app-dashboard',
   template: `
 <h3>Top Heroes</h3>
-<!--<div class="grid grid-pad">-->
-  <!--<div *ngFor="let hero of heroes" class="col-1-4">-->
-    <!--<div class="module hero">-->
-      <!--<h4>{{hero.name}}</h4>-->
-    <!--</div>-->
-  <!--</div>-->
-<!--</div>-->
+<div class="grid grid-pad">
+  <div *ngFor="let hero of heroes" class="col-1-4">
+    <div class="module hero">
+      <h4>{{hero.name}}</h4>
+    </div>
+  </div>
+</div>
 `
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
   }
 
 }
